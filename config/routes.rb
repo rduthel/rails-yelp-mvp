1220a6bd4ccb63313e952be379662ffff64f1bcc
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-14 16:37:12 +0100

    removed unwanted and duplicated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    resources :reviews
+    resources :reviews, only: %i[new create]
   end
-
-  resources :reviews
 
   root to: 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
