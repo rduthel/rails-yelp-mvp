5b26ee0d65e5b33ace28c198b4dcb37f4cf76669
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-14 15:25:14 +0100

    has_many :reviews, dependent: :destroy

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,5 +5,5 @@ class Restaurant < ApplicationRecord
   validates :category,
             inclusion: { in: %w[chinese italian japanese french belgian] }
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
